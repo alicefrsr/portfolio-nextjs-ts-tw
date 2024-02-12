9a6c2f8f08834dfffe61185317276b39c058a1c4
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2024-02-12 14:50:51 +0100

    Set up + data: projects, skills

---
+++ lib/skillsData.ts
@@ -0,0 +1,62 @@
+export const skillsDaily = [
+  'MacOS',
+  'VS Code',
+  'Git',
+  'Github',
+  'Command Line',
+  'HTML5',
+  'CSS3',
+  'Tailwindcss',
+  'Javascript',
+  'React',
+  'React Testing Library',
+];
+
+export const skillsRegular = [
+  'CSS Modules',
+  'React Router',
+  'Tanstack Query',
+  'Redux',
+  'RESTful APIs',
+  'Postman',
+  'Node.js',
+  'npm',
+  'Express',
+  'MongoDB',
+  'Responsive',
+  'Web accessibility',
+  'WCAG',
+  'Axe DevTools',
+  // 'axe Accessibility Linter',
+  'axe-core-react',
+];
+
+export const skillsIntro = [
+  'Linux',
+  'Windows',
+  'Java',
+  'Swift',
+  'SwiftUI',
+  'Python',
+  'Bootstrap',
+  'Styled Components',
+  'Supabase',
+];
+
+export const skillsNow = [
+  'React Testing Library',
+  'Jest / Vitest',
+  'Typescript',
+  'Python',
+  'Web accessibility',
+];
+// const skillsToDo = [
+//   'React Native',
+//   'Testing',
+//   'CI/CD',
+//   'Jest',
+//   'Cypress',
+//   'Jenkins',
+//   'Solidity',
+// ];
+export const languages = ['English', 'French', 'Spanish'];
