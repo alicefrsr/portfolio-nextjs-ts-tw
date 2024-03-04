2c93446b52599c70f880559a2780b1379ac2535a
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2024-03-04 18:12:07 +0100

    test 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import About from '@/components/About';
-import Experience from '@/components/Experience';
+// import Experience from '@/components/Experience';
 import Intro from '@/components/Intro';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
