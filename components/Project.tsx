55ba24e221aa26f019ff17a25f20f14c218d4fb2
alicefrsr <94556600+alicefrsr@users.noreply.github.com>
2024-07-22 00:57:04 +0200

    fix typos

--- components/Project.tsx
+++ components/Project.tsx
@@ -70,18 +70,16 @@ export default function Project({
           </ul>
           {/* External links */}
           <div className='mt-4 flex gap-2'>
-            <ToolTip>
-              <a
-                href={demoURL}
-                target='_blank'
-                rel='noreferrer'
-                title='Live demo (Opens in new tab)'
-                className='flex items-center hover:cursor-pointer outline-none focus:text-focusColor hover:text-focusColor active:text-focusColor duration-200'
-              >
-                <LuExternalLink size={25} />
-                <span className='sr-only'>Live demo. Opens in new tab</span>
-              </a>
-            </ToolTip>
+            <a
+              href={demoURL}
+              target='_blank'
+              rel='noreferrer'
+              title='Live demo (Opens in new tab)'
+              className='flex items-center hover:cursor-pointer outline-none focus:text-focusColor hover:text-focusColor active:text-focusColor duration-200'
+            >
+              <LuExternalLink size={25} />
+              <span className='sr-only'>Live demo. Opens in new tab</span>
+            </a>
 
             <a
               href={codeURL}
